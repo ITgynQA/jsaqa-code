85d00e32dbb6822e4aa94cefbb65c5da103c9d1c
Ekaterina Evdokimova <kate_evd79@mail.ru>
2023-09-17 12:01:29 +0400

    Add test and coverage

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,41 @@
 const sorting = require("../../app");
 
+const config = require("./testCoverage");
+
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
-  });
+    it("Books names should be sorted in ascending order", () => {
+        const input = [
+          "Гарри Поттер",
+          "Властелин Колец",
+          "Волшебник изумрудного города",
+        ]
+
+        const expected = [
+          "Властелин Колец",
+          "Волшебник изумрудного города",
+          "Гарри Поттер",
+        ]
+
+        const result = sorting.sortByName(input)
+
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("Books names test suit", () => {
+    it("Books names should not be sorted", () => {
+        const input = [
+          "Гарри Поттер",
+          "Гарри Поттер",
+        ]
+
+        const expected = [
+          "Гарри Поттер",
+          "Гарри Поттер",
+        ]
+
+        const result = sorting.sortByName(input)
+
+        expect(result).toEqual(expected);
+    });
 });
